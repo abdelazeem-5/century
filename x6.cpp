944045e6f0889be39625e82724dc87baaef04ab8
abdelazeem-5 <154232833+abdelazeem-5@users.noreply.github.com>
2024-03-25 22:40:30 +0200

    Add files via upload

---
+++ x6.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{ 
+  int year;
+  int a;
+  cout << "enter year\n";
+  cin >> year;
+  a = year / 100;
+  cout << "century ";
+  cout << a;
+
+    return 0;
+}
